24436154ab1342a53649db61eb3d2978964974ff
Lucas Jandrew <fapiko@gmail.com>
2012-12-05 01:32:17 -0800

    Testing GH commit hook take 2

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,5 @@ maintainer_email "lucas@jandrew.com"
 license          "Apache 2.0"
 description      "Installs/Configures stud"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
